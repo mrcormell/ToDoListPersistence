99fcbd65cc48b278704d65dd5c6057d28ab7cd8e
David Cormell <d.cormell@etoncollege.org.uk>
2025-02-28 10:43:03 +0000

    Pre-vide on UserDefaults

--- ToDoListPersistence/ContentView.swift
+++ ToDoListPersistence/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isDarkMode: Bool = false
+    @State private var showingAddSheet: Bool = false
+    @State private var todos: [String] = ["Pick up dry cleaning", "Book an online groceries delivery", "File tax return"]
+    @State private var newTodo = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(todos, id: \.self) { todo in
+                Text(todo)
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing, content: {
+                    Button("Add", systemImage: "plus") {
+                        showingAddSheet = true
+                    }
+                })
+            }
+            .popover(isPresented: $showingAddSheet) {
+                Form {
+                    TextField("Add new ToDo", text: $newTodo)
+                    Button("Add") {
+                        todos.append(newTodo)
+                        newTodo = ""
+                        showingAddSheet = false
+                    }
+                }
+            }
+            Toggle("Dark Mode", isOn: $isDarkMode)
+                .padding()
         }
-        .padding()
+        .preferredColorScheme(isDarkMode ? .dark : .light)
+
     }
 }
 
